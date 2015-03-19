983563474dc1527943706ed192f2de44558a5764
Jeremy Leggat <jleggat@asu.edu>
2015-03-18 22:46:43 -0700

    ASU header
    Load ASU header.

--- header.php
+++ header.php
@@ -14,14 +14,25 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+<?php echo file_get_contents('http://www.asu.edu/asuthemes/4.3/heads/default.shtml'); ?><!-- ASU Header -->
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+<?php echo file_get_contents('http://www.asu.edu/asuthemes/4.3/includes/gtm.shtml'); ?><!-- ASU Google Tag Manager -->
+
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'jrnopswp' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
+		<div id="asu_header">
+			<?php echo file_get_contents('http://www.asu.edu/asuthemes/4.3/headers/default.shtml'); ?><!-- ASU Header -->
+
+		</div><!-- .asu_header -->
+
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
