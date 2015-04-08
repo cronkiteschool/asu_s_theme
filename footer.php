3efde2daf7bcd5ad97d4df63727939a53781a85e
Jeremy Leggat <jleggat@asu.edu>
2015-04-08 15:38:53 -0700

    Wordpress ASU them footer changes.

--- footer.php
+++ footer.php
@@ -12,14 +12,11 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'jrnopswp' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'jrnopswp' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'jrnopswp' ), 'jrnopswp', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php echo file_get_contents('http://www.asu.edu/asuthemes/4.3/includes/footer.shtml'); ?><!-- ASU Footer -->
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<?php echo file_get_contents('http://www.asu.edu/asuthemes/4.3/includes/footer.shtml'); ?><!-- ASU Footer -->
 <?php wp_footer(); ?>
 </body>
 </html>
