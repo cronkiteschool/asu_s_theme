59ad2a96a50aca8696f378172739e89b10f5bc52
Jeremy Leggat <jleggat@asu.edu>
2015-06-23 19:02:43 -0700

    remove post-formats support
    Not being used.

--- functions.php
+++ functions.php
@@ -66,9 +66,9 @@ function asu_s_setup() {
 	 * Enable support for Post Formats.
 	 * See http://codex.wordpress.org/Post_Formats
 	 */
-	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link',
-	) );
+   //add_theme_support( 'post-formats', array(*/
+		//'aside', 'image', 'video', 'quote', 'link',
+   //) );*/
 
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'asu_s_custom_background_args', array(
